b57933f9a7d2a3196b424b5e2f3edb6ac59a3bab
Krishadawut Olde Monnikhof <78374713+krishadawut-om@users.noreply.github.com>
2023-01-08 17:16:54 +0100

    Add files via upload

---
+++ models/cbam.py
@@ -0,0 +1,48 @@
+import torch
+import torch.nn as nn
+
+class ChannelAttention(nn.Module):
+    def __init__(self, channel, reduction=16):
+        super(ChannelAttention, self).__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.max_pool = nn.AdaptiveMaxPool2d(1)
+
+        self.fc = nn.Sequential(
+            nn.Linear(channel, channel // reduction, bias=False),
+            nn.ReLU(inplace=True),
+            nn.Linear(channel // reduction, channel, bias=False)
+        )
+
+    def forward(self, x):
+        avg_out = self.fc(self.avg_pool(x).squeeze(-1).squeeze(-1))
+        max_out = self.fc(self.max_pool(x).squeeze(-1).squeeze(-1))
+        out = avg_out + max_out
+        return torch.sigmoid(out).unsqueeze(-1).unsqueeze(-1)
+
+class SpatialAttention(nn.Module):
+    def __init__(self, kernel_size=7):
+        super(SpatialAttention, self).__init__()
+
+        assert kernel_size % 2 == 1, "Kernel size must be odd!"
+        padding = (kernel_size - 1) // 2
+
+        self.conv = nn.Conv2d(2, 1, kernel_size, padding=padding, bias=False)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        avg_out = torch.mean(x, dim=1, keepdim=True)
+        max_out, _ = torch.max(x, dim=1, keepdim=True)
+        x = torch.cat([avg_out, max_out], dim=1)
+        x = self.conv(x)
+        return self.sigmoid(x)
+
+class CBAM(nn.Module):
+    def __init__(self, channel, reduction=16):
+        super(CBAM, self).__init__()
+        self.channel_attention = ChannelAttention(channel, reduction)
+        self.spatial_attention = SpatialAttention()
+
+    def forward(self, x):
+        x = x * self.channel_attention(x)
+        x = x * self.spatial_attention(x)
+        return x
